916ae77a1411287db299ff6136163853c173dbda
Mateusz Łuczkowski <mateuszluczkowski@gmial.com>
2021-07-29 14:54:20 +0200

    Add scroll feature

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Hello world");
-
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener("wheel", (event) => {
-    console.log(event.wheelDelta);
+    if (isThrottled) return;
+    isThrottled = true;
+    setTimeout(() => (isThrottled = false), 1000);
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex += direction;
+    console.log(currentSectionIndex);
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
