4d96b53aa21244ca30193fbffc881c53b8a59cad
Mateusz Łuczkowski <mateuszluczkowski@gmial.com>
2021-07-29 20:17:34 +0200

    swiper and style Img

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const scroller = new Scroller("#root");
   document.addEventListener("wheel", scroller.listenScroll);
+
+  document.addEventListener("swipeUp", () => {
+    scroller.scroll(1);
+  });
+  document.addEventListener("swipeDown", () => {
+    scroller.scroll(-1);
+  });
 });
